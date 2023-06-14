3b7e391818c188ef07828127d23ada67dd0f931e
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-06-14 15:37:16 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -4,3 +4,6 @@ from multimodalReader import getImage
 st.write('File 1')
 images_array, imgscore = getImage('cold')
 st.write('File 2')
+st.write(images_array,imgscore)
+imagenamex = Image.open(images_array[0],"r")
+st.image(imagenamex,width=content_image_width)
